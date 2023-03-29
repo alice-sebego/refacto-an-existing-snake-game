0216d51e15b0653cf7a5971023377d0a09e52c67
Alice Sebego <alice.etvotrecom@gmail.com>
2023-03-29 10:23:14 +0200

    Initial commit

---
+++ assets/js/snake.js
@@ -0,0 +1,157 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var interval = 100;
+
+canvas.width = 400;
+canvas.height = 400;
+
+var score = 0;
+var displayScore = document.querySelector("#score");
+
+var snake = [
+  { x: 200, y: 200 },
+  { x: 190, y: 200 },
+  { x: 180, y: 200 },
+  { x: 170, y: 200 },
+  { x: 160, y: 200 },
+];
+
+var appleX = 100;
+var appleY = 100;
+
+function drawApple(appleX, appleY) {
+  ctx.fillStyle = "red";
+  ctx.fillRect(appleX, appleY, 10, 10);
+}
+
+function checkAppleCollision() {
+  var newAppleX = Math.floor((Math.random() * canvas.width) / 10) * 10;
+  var newAppleY = Math.floor((Math.random() * canvas.height) / 10) * 10;
+  for (var i = 0; i < snake.length; i++) {
+    if (newAppleX === snake[i].x && newAppleY === snake[i].y) {
+      return checkAppleCollision();
+    }
+  }
+  appleX = newAppleX;
+  appleY = newAppleY;
+}
+
+function updateScore() {
+  displayScore.innerHTML = `<strong>Score : ${score}</strong>`;
+}
+
+function drawSnake() {
+  for (var i = 0; i < snake.length; i++) {
+    ctx.fillStyle = "black";
+    ctx.fillRect(snake[i].x, snake[i].y, 10, 10);
+  }
+}
+var dx = 10;
+var dy = 0;
+var gameStarted = false;
+
+function startGame() {
+  snake = [
+    // premier = tête du serpent
+    { x: 200, y: 200 },
+    { x: 190, y: 200 },
+    { x: 180, y: 200 },
+    { x: 170, y: 200 },
+    { x: 160, y: 200 },
+  ];
+  dx = 10;
+  dy = 0;
+  appleX = 100;
+  appleY = 100;
+  gameStarted = true;
+  setTimeout(main, interval);
+}
+
+function moveSnake() {
+  var snakeHead = { x: snake[0].x + dx, y: snake[0].y + dy };
+  // When the snake eats the apple
+  if (appleX === snake[0].x && appleY === snake[0].y) {
+    checkAppleCollision();
+    snake.unshift(snakeHead);
+    // console.log(snake);
+    score++;
+  } else {
+    snake.pop();
+    snake.unshift(snakeHead);
+  }
+}
+
+function gameOver() {
+  alert("Game Over !");
+  gameStarted = false;
+}
+
+function checkCollision() {
+  if (
+    snake[0].x < 0 ||
+    snake[0].x >= canvas.width ||
+    snake[0].y < 0 ||
+    snake[0].y >= canvas.height
+  ) {
+    gameOver();
+  }
+  for (var i = 4; i < snake.length; i++) {
+    if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) {
+      gameOver();
+    }
+  }
+}
+
+document.addEventListener("keydown", function (event) {
+  if (!gameStarted) {
+    startGame();
+  } else {
+    switch (event.keyCode) {
+      // Left
+      case 37:
+        dx = -10;
+        dy = 0;
+        break;
+      // Up
+      case 38:
+        dx = 0;
+        dy = -10;
+        break;
+      // Right
+      case 39:
+        dx = 10;
+        dy = 0;
+        break;
+      // Down
+      case 40:
+        dx = 0;
+        dy = 10;
+        break;
+    }
+  }
+});
+
+function main() {
+  // Effacer le canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Mettre à jour la position du serpent
+  moveSnake();
+
+  // Vérifier les collisions
+  checkCollision();
+
+  // Dessiner le serpent
+  drawSnake();
+
+  // Dessiner la pomme
+  drawApple(appleX, appleY);
+
+  // Gérer l'affichage du score
+  updateScore();
+
+  if (gameStarted) {
+    // Répéter la boucle
+    setTimeout(main, interval);
+  }
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Refacto an existing snake game
+Take the code of an incomplete project and add additional features 
